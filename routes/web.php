0c441f5cb5b3a8db6f754cae428b3b8f359a1d97
IlyasaMufti <ilyasa.mufti@gmail.com>
2020-07-11 04:09:22 +0700

    fourth commit

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Route::get('blank', function () {
     return view('pages.blank');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
